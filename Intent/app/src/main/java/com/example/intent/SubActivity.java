947b0a0fc0b5fb031d134b7d7626bc59d69545d7
Moonvely_i <68009161+dreamjh20@users.noreply.github.com>
2020-09-07 20:27:20 +0900

    Intent
    it

---
+++ Intent/app/src/main/java/com/example/intent/SubActivity.java
@@ -0,0 +1,30 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SubActivity extends AppCompatActivity {
+
+
+    private Button go_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        go_back = findViewById(R.id.go_back);
+        go_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
