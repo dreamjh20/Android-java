b8b0b41c59b35364df96b18ad83b67205f6b271a
Moonvely_i <68009161+dreamjh20@users.noreply.github.com>
2020-11-04 20:26:12 +0900

    third

---
+++ VOICE_ANDROID_RESTART/app/src/main/java/com/example/voice_android_restart/RetrofitInterface.java
@@ -0,0 +1,8 @@
+package com.example.voice_android_restart;
+
+import retrofit2.http.POST;
+
+public interface RetrofitInterface {
+    //@POST("login")
+
+}
---
+++ VOICE_ANDROID_RESTART/app/src/main/java/com/example/voice_android_restart/Sign_up.java
@@ -0,0 +1,18 @@
+package com.example.voice_android_restart;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Sign_up extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
